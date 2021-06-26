6a734df8b8a04db9ec61eff9737a3fea0d238123
Igor de Brito Coura <igor-coura@hotmail.com>
2021-06-26 14:55:25 -0300

    Create Cargos.java

---
+++ Atividade_02_Linguagens_de_programação/src/br/com/IgorCoura/Atividade01/Enum/Cargos.java
@@ -0,0 +1,24 @@
+package br.com.IgorCoura.Atividade01.Enum;
+
+public enum Cargos  {
+    MobileMembers("Happy Coding!", "Happy_C0d1ng. Maskers"),
+    HeavyLifters("Podem contar conosco!","N00b_qu3_n_Se_r3pita.bat"),
+    ScriptGuys("Prazer em ajudar novos amigos de código!","QU3Ro_S3us_r3curs0s.py"),
+    BigBrothers("Sempre ajudando as pessoas membros ou não S2!", "...");
+
+    private String normal;
+    private String extra;
+
+    Cargos(String normal, String extra) {
+        this.normal = normal;
+        this.extra = extra;
+    }
+
+    public String getNormalMessage(){
+        return normal;
+    }
+
+    public String getExtraMessage(){
+        return extra;
+    }
+}
