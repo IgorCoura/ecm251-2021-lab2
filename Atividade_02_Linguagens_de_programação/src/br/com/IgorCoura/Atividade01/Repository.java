58a50403ef6bb3db538ceef00f92bb4b270bcf13
Igor de Brito Coura <igor-coura@hotmail.com>
2021-06-26 14:56:07 -0300

    Update Repository.java

--- Atividade_02_Linguagens_de_programação/src/br/com/IgorCoura/Atividade01/Repository.java
+++ Atividade_02_Linguagens_de_programação/src/br/com/IgorCoura/Atividade01/Repository.java
@@ -1,17 +1,24 @@
 package br.com.IgorCoura.Atividade01;
 
+import br.com.IgorCoura.Atividade01.Interface.IRepository;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Repository {
-    List<MembersSociety> list = ArrayList<MembersSociety>();
+public class Repository implements IRepository {
+
+    private List<MembersSociety> list = new ArrayList<MembersSociety>();
+
     public void insert(MembersSociety member){
         list.add(member);
     }
 
-    public void remove(MembersSociety member){
+    public void delete(MembersSociety member){
         list.remove(member);
     }
 
-    
+    public List<MembersSociety> selectList(){
+        return list;
+    }
+
 }
