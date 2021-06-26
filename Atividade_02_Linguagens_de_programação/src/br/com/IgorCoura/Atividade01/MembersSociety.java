56556ade1281c3eaf78627dff50152b9078bf22c
Igor de Brito Coura <igor-coura@hotmail.com>
2021-06-26 14:55:57 -0300

    Update MembersSociety.java

--- Atividade_02_Linguagens_de_programação/src/br/com/IgorCoura/Atividade01/MembersSociety.java
+++ Atividade_02_Linguagens_de_programação/src/br/com/IgorCoura/Atividade01/MembersSociety.java
@@ -1,14 +1,29 @@
 package br.com.IgorCoura.Atividade01;
 
+import br.com.IgorCoura.Atividade01.Enum.Cargos;
+import br.com.IgorCoura.Atividade01.Enum.Horarios;
+
 public class MembersSociety {
     private String nome;
     private String email;
     private Cargos cargo;
 
     public MembersSociety(String nome, String email, Cargos cargo){
-
+        this.nome = nome;
+        this.email = email;
+        this.cargo = cargo;
     }
 
+    public String getMessage(Horarios horario){
+        if(horario == Horarios.Normal){
+            return cargo.getNormalMessage();
+        }else {
+            return  cargo.getExtraMessage();
+        }
+    }
 
+    public String getDescription(Horarios horario){
+        return nome + " "+ email + " "+ cargo.toString() + " "+ getMessage(horario);
+    }
 
 }
