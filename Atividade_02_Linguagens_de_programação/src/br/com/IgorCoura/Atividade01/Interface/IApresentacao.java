c46fc45f2613847df61b51a84123895541effa89
Igor de Brito Coura <igor-coura@hotmail.com>
2021-06-26 18:16:05 -0300

    Create IApresentacao.java

---
+++ Atividade_02_Linguagens_de_programação/src/br/com/IgorCoura/Atividade01/Interface/IApresentacao.java
@@ -0,0 +1,7 @@
+package br.com.IgorCoura.Atividade01.Interface;
+
+import br.com.IgorCoura.Atividade01.Enum.Horarios;
+
+public interface IApresentacao {
+    public String getDescription(Horarios horario);
+}
