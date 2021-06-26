ad6ec1e03133a3268ac4c74c1b1713244ce0ef3b
Igor de Brito Coura <igor-coura@hotmail.com>
2021-06-26 16:35:13 -0300

    Update Repository.java

--- Atividade_02_Linguagens_de_programação/src/br/com/IgorCoura/Atividade01/Repository.java
+++ Atividade_02_Linguagens_de_programação/src/br/com/IgorCoura/Atividade01/Repository.java
@@ -4,13 +4,16 @@ import br.com.IgorCoura.Atividade01.Interface.IRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 public class Repository implements IRepository {
-
+    private static int id = 0;
     private List<MembersSociety> list = new ArrayList<MembersSociety>();
 
     public void insert(MembersSociety member){
+        member.setId(id);
         list.add(member);
+        id++;
     }
 
     public void delete(MembersSociety member){
@@ -19,6 +22,21 @@ public class Repository implements IRepository {
 
     public List<MembersSociety> selectList(){
         return list;
+    }
+
+    public MembersSociety select(int id){
+        var member = getMember((m)->{return m.getId() == id;});
+        return member;
+    }
+
+    private MembersSociety getMember(Function<MembersSociety, Boolean> pre){
+        MembersSociety contaDestino = null;             //Criando contaDestino
+        for(MembersSociety c : list){
+            if(pre.apply(c)){                  //Procura a conta com a função passada como parametro
+                contaDestino = c;
+            }
+        }
+        return contaDestino;                    //Retorna a conta
     }
 
 }
