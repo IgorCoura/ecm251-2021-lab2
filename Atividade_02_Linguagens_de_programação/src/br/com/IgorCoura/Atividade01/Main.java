6253be69204071e05cc0b42e41822ace138707ba
Igor de Brito Coura <igor-coura@hotmail.com>
2021-06-26 16:35:29 -0300

    Update Main.java

--- Atividade_02_Linguagens_de_programação/src/br/com/IgorCoura/Atividade01/Main.java
+++ Atividade_02_Linguagens_de_programação/src/br/com/IgorCoura/Atividade01/Main.java
@@ -1,6 +1,7 @@
 package br.com.IgorCoura.Atividade01;
 
 import br.com.IgorCoura.Atividade01.Enum.Cargos;
+import br.com.IgorCoura.Atividade01.Enum.Horarios;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,48 +13,109 @@ public class Main {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) {
-	    var repository = new Repository();
-	    var member = new MembersSociety("Rola", "Emial@rola", Cargos.BigBrothers);
-	    repository.insert(member);
-	    repository.insert(new MembersSociety("Cola", "Emial@cola", Cargos.MobileMembers));
-	    repository.delete(member);
-        var list = repository.selectList();
-    }
-
-    public static void displayMain() throws IOException {
-
-        String horarioTrabalho = "Extra";
-        System.out.println("Bem - Vindo ao MAsK_S0c13ty");
-        System.out.println("Horario de trabalho: "+ horarioTrabalho);
-        System.out.println("1 - Listar membros");
-        System.out.println("2 - Cadastrar membro");
-        System.out.println("3 - Excluir membro");
-        System.out.println("4 - Consultar membro");
-        System.out.println("5 - Trocar horario de trabalho");
-        System.out.println("0 - Sair");
-        String op = br.readLine();
-        switch (op){
-            case "1": diplayListarMembros();
-            break;
+        try{
+            displayMain();
         }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
     }
 
-    public static void diplayListarMembros(){
+    public static void displayMain() throws IOException, InterruptedException {
+        boolean loop = true;
+        while(loop){
+            String horarioTrabalho = "Extra";
+            System.out.println("Bem - Vindo ao MAsK_S0c13ty");
+            System.out.println("Horario de trabalho: "+ horarioTrabalho);
+            System.out.println("1 - Listar membros");
+            System.out.println("2 - Cadastrar membro");
+            System.out.println("3 - Excluir membro");
+            System.out.println("4 - Consultar membro");
+            System.out.println("5 - Trocar horario de trabalho");
+            System.out.println("0 - Sair");
+            String op = br.readLine();
+            switch (op){
+                case "1": diplayListarMembros();
+                    break;
+                case "2": displayCadastro();
+                    break;
+                case "3": displayExcluir();
+                    break;
+                case "4": displayConsultarMembro();
+                    break;
+                case "5": displayTrocarHorario();
+                break;
+                default:
+                    loop = false;
+            }
+        }
+
+    }
+
+
+
+    public static void diplayListarMembros() throws IOException, InterruptedException {
         var list = service.listMembers();
+        System.out.println("\nLista de Membros: ");
         for (var member: list) {
             System.out.println(member.getDescription(service.getHorarios()));
         }
+        System.out.println();
     }
 
     public static void displayCadastro() throws IOException {
+        System.out.println("\nCadastro de membros. \n");
         System.out.println("Nome: ");
         String nome = br.readLine();
         System.out.println("Email: ");
         String email = br.readLine();
-        System.out.println("Cargos: 1 - MobileMembers, 2 - HeavyLifters, 3 - ScriptGuys, 4 - BigBrothers.");
+        System.out.println("Cargos: 1 - MobileMembers, 2 - HeavyLifters, 3 - ScriptGuys, default - BigBrothers.");
         System.out.println("Cargo: ");
-        
+        String numCargo = br.readLine();
+        Cargos cargo = Cargos.BigBrothers;
+        switch (numCargo){
+            case "1": cargo = Cargos.MobileMembers;
+            break;
+            case "2": cargo = Cargos.HeavyLifters;
+            break;
+            case "3": cargo = Cargos.ScriptGuys;
+            break;
+        }
+        service.createMember(nome, email, cargo);
+        System.out.println();
     }
 
+    public static void displayExcluir() throws IOException {
+        System.out.println("\nDigite o id do membro que deseja deletar: ");
+        var id = Integer.parseInt(br.readLine());
+        service.RemoveByID(id);
+        System.out.println();
+    }
+
+    public static void displayConsultarMembro() throws IOException {
+        System.out.println("\nDigite o id do membro: ");
+        var id = Integer.parseInt(br.readLine());
+        var member = service.recoverMember(id);
+        System.out.println(member.getDescription(service.getHorarios()));
+        System.out.println();
+    }
+
+    public static void displayTrocarHorario() throws IOException {
+        var horario = Horarios.Normal;
+        System.out.println("\ndefault - Normal, 1 - Extra");
+        System.out.println("Trocar horario para: ");
+        try{
+            var num = Integer.parseInt(br.readLine());
+            if (num == 1){
+                horario = Horarios.Extra;
+            }
+        }catch (Exception e) {
+        }
+        finally {
+            service.setHorarios(horario);
+            System.out.println();
+        }
+    }
 
 }
