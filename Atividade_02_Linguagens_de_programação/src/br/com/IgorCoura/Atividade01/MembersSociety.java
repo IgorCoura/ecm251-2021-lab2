6d5a1d6b98434cfcd4fd9ee3bf2ccb7b07b6130c
Igor de Brito Coura <igor-coura@hotmail.com>
2021-06-26 13:29:44 -0300

    Create MembersSociety.java

---
+++ Atividade_02_Linguagens_de_programação/src/br/com/IgorCoura/Atividade01/MembersSociety.java
@@ -0,0 +1,14 @@
+package br.com.IgorCoura.Atividade01;
+
+public class MembersSociety {
+    private String nome;
+    private String email;
+    private Cargos cargo;
+
+    public MembersSociety(String nome, String email, Cargos cargo){
+
+    }
+
+
+
+}
