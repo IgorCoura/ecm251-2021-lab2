ec49a333891d5c8bda58a74af67271e4210dc432
Igor de Brito Coura <igor-coura@hotmail.com>
2021-06-27 03:15:30 -0300

    Doc IService

--- Atividade_02_Linguagens_de_programação/src/br/com/IgorCoura/Atividade01/Interface/IService.java
+++ Atividade_02_Linguagens_de_programação/src/br/com/IgorCoura/Atividade01/Interface/IService.java
@@ -8,10 +8,42 @@ import java.util.List;
 
 public interface IService {
 
-    void setHorarios(Horarios horarios);
+    /**
+     * Este metodo muda o horario de trabalho dos membros.
+     * @param horario Horarios - horario para qual será alterando.
+     */
+    void setHorarios(Horarios horario);
+
+    /**
+     * Este metodo returna o atual horario de trabalho dos membros.
+     * @return Horarios - horarios atual de trabalho.
+     */
     Horarios getHorarios();
+
+    /**
+     * Este metodo cria um novo membro e inseri no repository.
+     * @param nome String - Nome do membro.
+     * @param email String - Email do membro.
+     * @param cargo Cargos - Cargo a qual o membro ocupa.
+     */
     void createMember(String nome, String email, Cargos cargo);
+
+    /**
+     * Este metodo busca todos os membros cadastrados no repository.
+     * @return List<MembersSociety> - Lista dos membros da sociedade.
+     */
     List<MembersSociety> listMembers();
+
+    /**
+     * Este metodo busca um membro no repository atraves do seu id.
+     * @param id int - id do membro o qual deseja buscar
+     * @return MembersSociety - membro da sociedade.
+     */
     MembersSociety recoverMember(int id);
+
+    /**
+     * Este metodo remove um membro do repository atraves do seu id.
+     * @param id int - Id do membro o qual sera removido.
+     */
     void RemoveByID(int id);
 }
