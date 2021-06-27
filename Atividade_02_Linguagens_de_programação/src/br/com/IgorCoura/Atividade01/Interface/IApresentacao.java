a779b12f22cc7145a9216b5b63f5ce7d1329534a
Igor de Brito Coura <igor-coura@hotmail.com>
2021-06-27 03:14:36 -0300

    Doc IApresentacao

--- Atividade_02_Linguagens_de_programação/src/br/com/IgorCoura/Atividade01/Interface/IApresentacao.java
+++ Atividade_02_Linguagens_de_programação/src/br/com/IgorCoura/Atividade01/Interface/IApresentacao.java
@@ -3,5 +3,11 @@ package br.com.IgorCoura.Atividade01.Interface;
 import br.com.IgorCoura.Atividade01.Enum.Horarios;
 
 public interface IApresentacao {
+    /**
+     * Este metodo devolve um descricao completa do membro com id, nome, email, cargo, horarios de trabalho e mensagem
+     * de acordo com o horarios passado como parametro.
+     * @param horario Horarios - horario de trabalho.
+     * @return String - descricao do membro.
+     */
     public String getDescription(Horarios horario);
 }
