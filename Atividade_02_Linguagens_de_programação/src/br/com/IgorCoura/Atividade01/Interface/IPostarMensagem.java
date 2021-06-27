a2bda491b84e10118323d987c79e3e0de69c6c51
Igor de Brito Coura <igor-coura@hotmail.com>
2021-06-27 03:14:54 -0300

    Doc IPostarMensagem

--- Atividade_02_Linguagens_de_programação/src/br/com/IgorCoura/Atividade01/Interface/IPostarMensagem.java
+++ Atividade_02_Linguagens_de_programação/src/br/com/IgorCoura/Atividade01/Interface/IPostarMensagem.java
@@ -3,6 +3,16 @@ package br.com.IgorCoura.Atividade01.Interface;
 import br.com.IgorCoura.Atividade01.Enum.Horarios;
 
 public interface IPostarMensagem {
+    /**
+     * Este metodo devolve o assinatura atual de trabalha de acordo com o horario passado.
+     * @param horario Horarios - horarios de trabalho.
+     * @return
+     */
     String getMessage(Horarios horario);
+    /**
+     * Este metodo devolve o post atual de trabalha de acordo com o horario passado.
+     * @param horario Horarios - horarios de trabalho.
+     * @return
+     */
     void postMessage(Horarios horario);
 }
